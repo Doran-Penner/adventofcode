9f00ba714620523387bf84bb4f93fb1fd8824f54
Doran Penner <doranaviv@gmail.com>
2024-12-02 12:09:32 +0100

    2.2 done, but not pretty

--- 2024/racket/02.rkt
+++ 2024/racket/02.rkt
@@ -17,11 +17,11 @@
   (if (equal? line '())
       (and jumps-small (or all-incr all-decr))
       (line-is-safe
-        (cdr line)
-        (car line)
-        (and all-incr (< prev (car line)))
-        (and all-decr (> prev (car line)))
-        (and jumps-small (<= 1 (abs (- prev (car line))) 3)))))
+       (cdr line)
+       (car line)
+       (and all-incr (< prev (car line)))
+       (and all-decr (> prev (car line)))
+       (and jumps-small (<= 1 (abs (- prev (car line))) 3)))))
 
 (define (line-folder line acc)
   (if (line-is-safe (cdr line) (car line) #t #t #t)
@@ -36,3 +36,34 @@
 (printf result)
 |#
 ; WAHOO
+
+
+; incredibly silly and inefficient but whateverrrrr
+; make a list of lines, each with one item missing
+
+(define (remove-at-index i list)
+  (if (<= i 0)
+      (cdr list)
+      (cons (car list)
+            (remove-at-index (- i 1) (cdr list)))))
+
+(define (gen-dampened-lines line)
+  (build-list (length line)
+              (lambda (n)
+                (remove-at-index n line))))
+
+(define (check-maker line)
+  (line-is-safe (cdr line) (car line) #t #t #t))
+
+(define (line-folder/2dumb line acc)
+  (if (ormap check-maker (cons line (gen-dampened-lines line)))
+      (+ acc 1)
+      acc))
+
+(define answer2 (foldl line-folder/2dumb 0 lines))
+
+#|
+(define result
+  (aoc-submit sesh 2024 02 2 answer2))
+(printf result)
+|#
