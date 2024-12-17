dbde722060242f325231ef4d46264b2e84d60aaa
Doran Penner <doranaviv@gmail.com>
2024-12-16 19:20:05 -0800

    done with day 5 (and racket too)

--- 2024/racket/05.rkt
+++ 2024/racket/05.rkt
@@ -58,3 +58,102 @@
 (printf result)
 |#
 ; WAHOO
+
+
+; This is a "known" problem: finding a topological sorting
+; As such we will be lazy and use wikipedia's formulation
+; of Kahn's algorithm for this problem
+; Work smart, not hard!
+
+#|
+S: set of int,
+  starts with all possible "starting" nodes
+L: list of int,
+  starts empty and is our final solution (once reversed)
+(hidden graph) G: mutable hash of (int, (set of int)),
+  newly-created for each line (filter for relevant rules)
+|#
+
+(define (pair->set pair)
+  (set (car pair) (cdr pair)))
+
+(define (relevant-rules instruction/lst)
+  (define (rule-is-relevant? rule/pair)
+    (equal? 2
+            (set-count (set-intersect (list->set instruction/lst)
+                                      (pair->set rule/pair)))))
+  (filter rule-is-relevant? (set->list rules)))
+
+; so much work to make immutable hash from list
+(define (initial-hash-set instruction)
+  (apply hash
+         (flatten (map (lambda (x) (cons x (set))) instruction))))
+
+(define (graph-of instruction)
+  (define init
+    (initial-hash-set instruction))
+  (define rel-rules
+    (relevant-rules instruction))
+  (foldl
+   (lambda (new-rule acc-hash)
+     (hash-update
+      acc-hash
+      (car new-rule)
+      (lambda (prev-set)
+        (set-add prev-set (cdr new-rule)))))
+   init
+   rel-rules))
+
+(define (starting-nodes graph)
+  (foldl
+   (lambda (st2 acc) (set-subtract acc st2))
+   (list->set (hash-keys graph))
+   (hash-values graph)))
+
+(define (kahns-loop/inner graph n safe-nodes)
+  (if (set-empty? (hash-ref graph n)) (cons graph safe-nodes)
+      ; else, pop some edge from graph[n] and
+      ; insert it into safe-nodes if it's now safe
+      (let ([m (set-first (hash-ref graph n))]
+            [new-graph (hash-update graph n set-rest)])
+        (kahns-loop/inner
+         new-graph
+         n
+         (if (set-member?
+              (foldl
+               set-union
+               (set)
+               (hash-values new-graph)) m)
+             safe-nodes
+             (set-add safe-nodes m))))))
+
+(define (kahns-loop/outer graph safe-nodes l)
+  (if (set-empty? safe-nodes) l
+      (let* ([n (set-first safe-nodes)]
+             [new-l (cons n l)]
+             ; we need to update safe-nodes inside the new-graph loop
+             ; so we need to return both the graph and safe nodes
+             [graph-safe/pair (kahns-loop/inner graph n (set-rest safe-nodes))])
+        (kahns-loop/outer (car graph-safe/pair) (cdr graph-safe/pair) new-l))))
+
+(define (ordering instruction)
+  (define graph (graph-of instruction))
+  (define starts (starting-nodes graph))
+  (reverse (kahns-loop/outer graph starts '())))
+
+; reduce (map (map (filter)))
+(define answer2
+  (foldl + 0
+         (map get-middle-number
+              (map ordering
+                   (filter
+                    (lambda (instr)
+                      (not (valid-instruction? instr)))
+                    instructions)))))
+
+#|
+(define result
+  (aoc-submit sesh 2024 05 2 answer2))
+(printf result)
+|#
+; I'm so done with racket
