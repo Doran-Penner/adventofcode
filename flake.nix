3f442ff1fac207031f178aaa8269cc6de9015851
Doran Penner <doranaviv@gmail.com>
2024-12-24 22:41:37 -0800

    Use fenix
    By using fenix (rust toolchain manager provided by
    nix-community), we get an easy offline docs link
    and can remove the hacky openssl config.

--- flake.nix
+++ flake.nix
@@ -3,24 +3,31 @@
 
 	inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
+	inputs.fenix.url = "github:nix-community/fenix/monthly";
+	inputs.fenix.inputs.nixpkgs.url = "nixpkgs";
+
 	outputs = {
 		self,
 		nixpkgs,
+		fenix,
 	}: let
 		system = "x86_64-linux";
 		pkgs = import nixpkgs {inherit system;};
 	in {
 		devShells.${system}.default = pkgs.mkShell {
-			packages = with pkgs; [
-				# remember to `raco pkg install racket-langserver`!
-				racket
-				# rust time
-				cargo
-				rustc
-			];
-			buildInputs = [ pkgs.openssl ];
-			nativeBuildInputs = [ pkgs.pkg-config ];
-			LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [ pkgs.openssl ];
+			packages = let
+				rust-packages = fenix.packages.${system}.stable.defaultToolchain;
+        # nice link to offline docs
+				rust-docs = pkgs.writeShellScriptBin "rust-docs" ''
+					xdg-open "${rust-packages}/share/doc/rust/html/index.html"
+				'';
+			in
+				with pkgs; [
+					# remember to `raco pkg install racket-langserver`!
+					racket
+					rust-packages
+					rust-docs
+				];
 		};
 	};
 }
